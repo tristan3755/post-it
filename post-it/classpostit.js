defb6e642e2e84712cfb12bfa172e550fee320cb
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-06-12 15:20:13 +0200

    postit

--- post-it/classpostit.js
+++ post-it/classpostit.js
@@ -10,11 +10,12 @@ class postIt {
     taille;
     largeurZoneText;
     hauteurZoneText;
+    contenu;
    
 
     
 
-    constructor(largeur,hauteur,x,y,couleurFond,couleurTexte,titre,taille,largeurZoneText,hauteurZoneText){
+    constructor(largeur,hauteur,x,y,couleurFond,couleurTexte,titre,taille,largeurZoneText,hauteurZoneText,contenu){
 
     this.y=y;
     this.x=x;
@@ -26,9 +27,7 @@ class postIt {
     this.taille=taille
     this.largeurZoneText=largeurZoneText
     this.hauteurZoneText=hauteurZoneText
-    
-    
-
+    this.contenu=contenu
     
     }
 
@@ -36,14 +35,23 @@ class postIt {
 
     affichePostit(){
 
-    let blocPostit = document.createElement('div');
-    blocPostit.id='post';
+    let blocPostit=document.getElementById('post')
+   
+
+    if(blocPostit===null){
+
+        blocPostit = document.createElement('div');
+        blocPostit.id='post';
+
+    }
+    
     blocPostit.style.width=this.largeur+"px";
     blocPostit.style.height=this.hauteur+"px";
     blocPostit.style.left=this.x+"px";
     blocPostit.style.top=this.y+"px";
     blocPostit.style.position="fixed";
     blocPostit.style.backgroundColor=this.couleurFond;
+    blocPostit.innerHTML=' '
     
 
     let monTitre = document.createElement('h1')
@@ -54,21 +62,92 @@ class postIt {
 
 
     let monText=document.createElement('textarea')
-    
+    monText.id='text';
     monText.style.width=this.largeurZoneText+'%';
     monText.style.height=this.hauteurZoneText+'%';
     monText.style.backgroundColor=this.couleurFond;
-    
-
+    monText.innerHTML = this.contenu;
     blocPostit.appendChild(monText)
 
+    let divBoutton = document.createElement('div');
+    divBoutton.className='divB'
 
+    let monBoutonBouge=document.createElement('button')
+    monBoutonBouge.className='bouton';
+    let maValue = document.createElement('i')
+    maValue.classList.add("fas")
+    maValue.classList.add( "fa-arrows-alt")
     
+
+    let monBoutonSupp=document.createElement('button')
+    monBoutonSupp.className='bouton';
+    let maValue1 = document.createElement('i')
+    maValue1.classList.add("fas")
+    maValue1.classList.add( "fa-trash-alt")
+    
+
+    let monBoutonRedimension=document.createElement('button')
+    monBoutonRedimension.className='bouton';
+    let maValue2 = document.createElement('i')
+    maValue2.classList.add("fas")
+    maValue2.classList.add("fa-expand-arrows-alt")
+    
+    
+
+
+    let monBoutonEdition=document.createElement('button')
+    monBoutonEdition.className='bouton';
+    let maValue3 = document.createElement('i')
+    maValue3.classList.add("fas")
+    maValue3.classList.add("fa-edit")
+    
+
+
+    monBoutonBouge.appendChild(maValue)
+    monBoutonSupp.appendChild(maValue1)
+    monBoutonRedimension.appendChild(maValue2)
+    monBoutonEdition.appendChild(maValue3)
+
+
+
+    divBoutton.appendChild(monBoutonBouge)
+    divBoutton.appendChild(monBoutonSupp)
+    divBoutton.appendChild(monBoutonRedimension)
+    divBoutton.appendChild(monBoutonEdition)
+    
+
+
+    blocPostit.appendChild(divBoutton)
+    
+
     document.getElementById('post-it').appendChild(blocPostit)
+
     
     }
 
 
- 
+
+
+    bougePostit(newX,newY){
+
+        this.x = newX
+        this.y =newY
+
+    }
+
+    taillePostit(newLargeur,newHauteur){
+
+        this.largeur=newLargeur
+        this.hauteur=newHauteur
+    }
+
+    modifContenu(newContenu){
+
+        this.contenu=newContenu
+
+    }
+
+
+
 
 }
--- post-it/javascript.js
+++ post-it/javascript.js
@@ -1,7 +1,12 @@
-let monPostit = new postIt (275,200,75,100,'yellow','black','Faire L\'exo',25,90,50)
+let monPostit = new postIt (275,200,75,100,'yellow','black','Faire L\'exo',25,90,50,'Notes')
 
 monPostit.affichePostit()
-
+monPostit.bougePostit(600,200)
+monPostit.taillePostit(500,200)
+monPostit.modifContenu('- trouver la gestion des addEventListener')
+monPostit.modifContenu(monPostit.contenu +'- faire virer julien -')
+monPostit.modifContenu(monPostit.contenu +'- retrouver Marc au PMU -')
+monPostit.affichePostit()
 
 
 
