294dca19888185cf778703a8324eca7ded68b191
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-06-19 12:22:48 +0200

    postit

--- post-it/javascript.js
+++ post-it/javascript.js
@@ -22,8 +22,51 @@ var xPos
 var yPos
 
 
+/*function setInterval(() => {
 
 
+    
+}, interval);*/
+
+
+/* code cours */
+
+document.querySelector(".cookies").addEventListener('click',(event)=>{
+
+    createCookie("test","bordel je ne comprends rien aux cookies",30)
+
+})
+
+
+window.addEventListener('load',(event)=>{
+
+console.log(readCookie("test")) 
+
+})
+
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+/* code cours */
 
 document.querySelector(".buttonOption2").addEventListener('click',(event)=>{
     
