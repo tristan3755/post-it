ef5035266f2c89541aabe086016e71f006bf4b67
FIRST_tristan LAST_richard <tristan3755@gmail.com>
2020-06-19 09:28:15 +0200

    post it

--- post-it/classpostit.js
+++ post-it/classpostit.js
@@ -1,210 +1,219 @@
-class postIt { 
-    
-    largeur; 
-    hauteur; 
+class postIt {
+
+    largeur;
+    hauteur;
     x;
     y;
     couleurFond;
     couleurTexte;
     titre;
     taille;
-    
     contenu;
     departX;
     departY;
-   
+    id;
 
-    
 
-    constructor(largeur,hauteur,x,y,couleurFond,couleurTexte,titre,taille,contenu){
 
-    this.y=y;
-    this.x=x;
-    this.hauteur=hauteur;
-    this.largeur=largeur;
-    this.couleurFond=couleurFond;
-    this.couleurTexte=couleurTexte;
-    this.titre=titre
-    this.taille=taille
-    
-    this.contenu=contenu
-    this.departX=0
-    this.departY=0
-    
+
+    constructor(largeur, hauteur, x, y, couleurFond, couleurTexte, titre, taille, contenu, id) {
+
+        this.y = y;
+        this.x = x;
+        this.hauteur = hauteur;
+        this.largeur = largeur;
+        this.couleurFond = couleurFond;
+        this.couleurTexte = couleurTexte;
+        this.titre = titre
+        this.taille = taille
+        this.id = id
+        this.contenu = contenu
+        this.departX = 0
+        this.departY = 0
+
     }
 
-    
 
-    affichePostit(){
 
-    let blocPostit=document.getElementById('post')
-   
+    affichePostit() {
 
-    if(blocPostit===null){
+        let blocPostit = document.getElementById('post' + this.id)
 
-        blocPostit = document.createElement('div');
-        blocPostit.id='post';
-        blocPostit.addEventListener('click',()=>{
-            
-            bougePost=!bougePost
-            resizePost=false
-            modif=false
-           
+
+
+        if (blocPostit === null) {
+
+            blocPostit = document.createElement('div');
+            blocPostit.id = 'post' + this.id;
+            blocPostit.addEventListener('click', () => {
+
+                bougePost = !bougePost
+                numPostit = this.id
+                resizePost = false
+                modif = false
+
+            })
+
+
+        }
+
+        blocPostit.style.width = this.largeur + "px";
+        blocPostit.style.height = this.hauteur + "px";
+        blocPostit.style.left = this.x + "px";
+        blocPostit.style.top = this.y + "px";
+        blocPostit.style.position = "absolute";
+        blocPostit.style.backgroundColor = this.couleurFond;
+        blocPostit.innerHTML = ' '
+
+        blocPostit.classList.add("mesPostit")
+
+        let monTitre = document.createElement('h1')
+        monTitre.style.color = this.couleurTexte;
+        monTitre.style.fontSize = this.taille + 'px';
+        monTitre.innerHTML = this.titre
+        blocPostit.appendChild(monTitre)
+
+
+        let monText = document.createElement('p')
+        monText.id = 'text';
+        monText.style.backgroundColor = this.couleurFond;
+        monText.innerHTML = this.contenu;
+        blocPostit.appendChild(monText)
+
+        let divBoutton = document.createElement('div');
+        divBoutton.className = 'divB'
+
+        let monBoutonBouge = document.createElement('button')
+        monBoutonBouge.className = 'bouton';
+        let maValue = document.createElement('i')
+        maValue.classList.add("fas")
+        maValue.classList.add("fa-arrows-alt")
+
+
+        let monBoutonSupp = document.createElement('button')
+        monBoutonSupp.className = 'bouton';
+        let maValue1 = document.createElement('i')
+        maValue1.classList.add("fas")
+        maValue1.classList.add("fa-trash-alt")
+
+
+        let monBoutonRedimension = document.createElement('button')
+        monBoutonRedimension.className = 'bouton';
+        let maValue2 = document.createElement('i')
+        maValue2.classList.add("fas")
+        maValue2.classList.add("fa-expand-arrows-alt")
+
+
+        monBoutonRedimension.addEventListener('click', (event) => {
+
+            this.departX = xPos
+            this.departY = yPos
+            this.largdep = this.largeur
+            this.hautdep = this.hauteur
+            resizePost = !resizePost
+            bougePost = false
+            numPostit = this.id
+            //console.log(x+' '+y)
+            event.stopPropagation()
+
         })
 
-     
-    }
-    
-    blocPostit.style.width=this.largeur+"px";
-    blocPostit.style.height=this.hauteur+"px";
-    blocPostit.style.left=this.x+"px";
-    blocPostit.style.top=this.y+"px";
-    blocPostit.style.position="absolute";
-    blocPostit.style.backgroundColor=this.couleurFond;
-    blocPostit.innerHTML=' '
-    
-
-    let monTitre = document.createElement('h1')
-    monTitre.style.color=this.couleurTexte;
-    monTitre.style.fontSize=this.taille+'px';
-    monTitre.innerHTML=this.titre
-    blocPostit.appendChild(monTitre)
 
 
-    let monText=document.createElement('p')
-    monText.id='text';
-    monText.style.backgroundColor=this.couleurFond;
-    monText.innerHTML = this.contenu;
-    blocPostit.appendChild(monText)
+        let monBoutonEdition = document.createElement('button')
+        monBoutonEdition.className = 'bouton';
+        let maValue3 = document.createElement('i')
+        maValue3.classList.add("fas")
+        maValue3.classList.add("fa-edit")
 
-    let divBoutton = document.createElement('div');
-    divBoutton.className='divB'
+        monBoutonEdition.addEventListener('click', (event) => {
 
-    let monBoutonBouge=document.createElement('button')
-    monBoutonBouge.className='bouton';
-    let maValue = document.createElement('i')
-    maValue.classList.add("fas")
-    maValue.classList.add( "fa-arrows-alt")
-    
-
-    let monBoutonSupp=document.createElement('button')
-    monBoutonSupp.className='bouton';
-    let maValue1 = document.createElement('i')
-    maValue1.classList.add("fas")
-    maValue1.classList.add( "fa-trash-alt")
-    
-
-    let monBoutonRedimension=document.createElement('button')
-    monBoutonRedimension.className='bouton';
-    let maValue2 = document.createElement('i')
-    maValue2.classList.add("fas")
-    maValue2.classList.add("fa-expand-arrows-alt")
-    
-    
-    monBoutonRedimension.addEventListener('click',(event)=>{
-            
-        this.departX=xPos
-       this.departY=yPos
-       this.largdep=this.largeur
-       this.hautdep=this.hauteur
-       resizePost=!resizePost
-        bougePost=false
-        //console.log(x+' '+y)
-       event.stopPropagation()
-       
-    })
+            modif = !modif
+            bougePost = false
+            event.stopPropagation()
+            //console.log(modif)
 
 
 
-    let monBoutonEdition=document.createElement('button')
-    monBoutonEdition.className='bouton';
-    let maValue3 = document.createElement('i')
-    maValue3.classList.add("fas")
-    maValue3.classList.add("fa-edit")
+        })
 
-    monBoutonEdition.addEventListener('click',(event)=>{
+        let monBoutonTitre = document.createElement('button')
+        monBoutonTitre.className = 'bouton';
+        let maValue4 = document.createElement('i')
+        maValue4.classList.add("fas")
+        maValue4.classList.add("fa-text-height")
 
-        modif=!modif
-        bougePost=false
-        event.stopPropagation()
-        //console.log(modif)
+        monBoutonTitre.addEventListener('click', (event) => {
 
-
-
-    })
-    
-let monBoutonTitre=document.createElement('button')
-monBoutonTitre.className='bouton';
-let maValue4 = document.createElement('i')
-maValue4.classList.add("fas")
-maValue4.classList.add("fa-text-height")
-
-monBoutonTitre.addEventListener('click',(event)=>{
-
-    titre=!titre
-    bougePost=false
-    event.stopPropagation()
-    //console.log(titre)
+            titre = !titre
+            bougePost = false
+            numPostit = this.id
+            event.stopPropagation()
+            //console.log(titre)
 
 
 
-})
+        })
 
 
-    monBoutonBouge.appendChild(maValue)
-    monBoutonSupp.appendChild(maValue1)
-    monBoutonRedimension.appendChild(maValue2)
-    monBoutonEdition.appendChild(maValue3)
-    monBoutonTitre.appendChild(maValue4)
+        monBoutonBouge.appendChild(maValue)
+        monBoutonSupp.appendChild(maValue1)
+        monBoutonRedimension.appendChild(maValue2)
+        monBoutonEdition.appendChild(maValue3)
+        monBoutonTitre.appendChild(maValue4)
 
 
 
-    divBoutton.appendChild(monBoutonBouge)
-    divBoutton.appendChild(monBoutonSupp)
-    divBoutton.appendChild(monBoutonRedimension)
-    divBoutton.appendChild(monBoutonEdition)
-    divBoutton.appendChild(monBoutonTitre)
-    
+        divBoutton.appendChild(monBoutonBouge)
+        divBoutton.appendChild(monBoutonSupp)
+        divBoutton.appendChild(monBoutonRedimension)
+        divBoutton.appendChild(monBoutonEdition)
+        divBoutton.appendChild(monBoutonTitre)
 
 
-    blocPostit.appendChild(divBoutton)
-    
 
-    document.getElementById('post-it').appendChild(blocPostit)
+        blocPostit.appendChild(divBoutton)
 
-    
+
+        document.getElementById('post-it').appendChild(blocPostit)
+
+
     }
 
 
 
 
-    bougePostit(newX,newY){
+    bougePostit(newX, newY) {
 
         this.x = newX
-        this.y =newY
+        this.y = newY
+       
 
+
+    }
+
+    taillePostit(newLargeur, newHauteur) {
+
+        this.largeur = newLargeur
+        this.hauteur = newHauteur
+        
+    }
+
+    modifContenu(newContenu) {
+
+        this.contenu = newContenu
         
 
-
     }
 
-    taillePostit(newLargeur,newHauteur){
+    modifTitre(newTitre) {
 
-        this.largeur=newLargeur
-        this.hauteur=newHauteur
-    }
-
-    modifContenu(newContenu){
-
-        this.contenu=newContenu
-
-    }
-
-    modifTitre(newTitre){
-
-        this.titre=newTitre
+        this.titre = newTitre
+        
 
     }
 
 
 }
+
+
--- post-it/javascript.js
+++ post-it/javascript.js
@@ -6,17 +6,86 @@ let modif=false
 
 let titre=false
 
+let creaJaune=false
+
+let tabPostIt=[]
+
+let numPostit
+
+
+//[a-zA-Z0-9]
+// document.cookie = ‘user=Julien’
+
 
 
 var xPos 
 var yPos
 
 
-let monPostit = new postIt (275,200,575,100,'yellow','black','A ne pas oublier!',25,'Notes')
-
-monPostit.affichePostit()
 
 
+
+document.querySelector(".buttonOption2").addEventListener('click',(event)=>{
+    
+    creaJaune=!creaJaune
+    console.log(creaJaune)
+    
+    let monPostit = new postIt (275,200,575,100,'yellow','black','A ne pas oublier!',25,'Notes',tabPostIt.length+1)
+    
+    tabPostIt.push(monPostit)
+    monPostit.affichePostit()
+   
+
+
+
+})
+
+document.querySelector(".buttonOption1").addEventListener('click',(event)=>{
+    
+    
+    
+    let monPostit = new postIt (275,200,275,100,'green','black','A ne pas oublier!',25,'Notes',tabPostIt.length+1)
+    
+    tabPostIt.push(monPostit)
+    monPostit.affichePostit()
+   
+
+
+
+})
+
+
+
+
+document.querySelector(".buttonOption").addEventListener('click',(event)=>{
+    
+    
+    
+    let monPostit = new postIt (275,200,675,100,'red','black','A ne pas oublier!',25,'Notes',tabPostIt.length+1)
+    
+    tabPostIt.push(monPostit)
+    monPostit.affichePostit()
+   
+
+
+
+})
+
+
+
+document.querySelector(".buttonOption3").addEventListener('click',(event)=>{
+    
+    
+    
+    let monPostit = new postIt (275,200,675,300,'darkblue','black','A ne pas oublier!',25,'Notes',tabPostIt.length+1)
+    
+    tabPostIt.push(monPostit)
+    monPostit.affichePostit()
+   
+
+
+
+})
 
 document.body.addEventListener('mousemove',(event)=>{
 
@@ -25,14 +94,14 @@ document.body.addEventListener('mousemove',(event)=>{
 
     
 if(bougePost){
-    monPostit.bougePostit(xPos-50,yPos-50)
-    monPostit.affichePostit()
+    tabPostIt[numPostit-1].bougePostit(xPos-50,yPos-50)
+    tabPostIt[numPostit-1].affichePostit()
 }
 
 if(resizePost){
 
-    monPostit.taillePostit((+monPostit.largdep)+(+xPos-monPostit.departX),(+monPostit.hautdep)+(+yPos-monPostit.departY))
-    monPostit.affichePostit()
+    tabPostIt[numPostit-1].taillePostit((+tabPostIt[numPostit-1].largdep)+(+xPos-tabPostIt[numPostit-1].departX),(+tabPostIt[numPostit-1].hautdep)+(+yPos-tabPostIt[numPostit-1].departY))
+    tabPostIt[numPostit-1].affichePostit()
     
 
 }
@@ -54,7 +123,7 @@ if(event.key=='Backspace'){
     
 //console.log(monPostit.contenu.substr(0,1))
 
-monPostit.modifContenu(monPostit.contenu.substr(0,monPostit.contenu.length-1))
+tabPostIt[numPostit-1].modifContenu( tabPostIt[numPostit-1].contenu.substr(0, tabPostIt[numPostit-1].contenu.length-1))
 
 
 
@@ -64,17 +133,17 @@ else if(event.key=='Enter'){
     
     //console.log(monPostit.contenu.substr(0,1))
     
-    monPostit.modifContenu(monPostit.contenu("<br>"))
+    tabPostIt[numPostit-1].modifContenu( tabPostIt[numPostit-1].contenu+"<br>")
     
     
     
     }
 
 else{
-    monPostit.modifContenu(monPostit.contenu + modifText)
+    tabPostIt[numPostit-1].modifContenu( tabPostIt[numPostit-1].contenu + modifText)
 }
 
-monPostit.affichePostit()
+tabPostIt[numPostit-1].affichePostit()
 
 }
 
@@ -94,17 +163,19 @@ document.body.addEventListener('keydown',(event)=>{
         
     //console.log(monPostit.contenu.substr(0,1))
     
-    monPostit.modifTitre(monPostit.titre.substr(0,monPostit.titre.length-1))
+    tabPostIt[numPostit-1].modifTitre( tabPostIt[numPostit-1].titre.substr(0, tabPostIt[numPostit-1].titre.length-1))
     
     
     
     }
     
     else{
-        monPostit.modifTitre(monPostit.titre + modifTitre)
+
+        tabPostIt[numPostit-1].modifTitre( tabPostIt[numPostit-1].titre + modifTitre)
+
     }
     
-    monPostit.affichePostit()
+    tabPostIt[numPostit-1].affichePostit()
     
     }
     
